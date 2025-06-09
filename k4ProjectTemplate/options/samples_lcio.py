f288fe351f0ee9b52ba4b5bd7330833ddc550643
CarstenHensel <hensel.carsten@gmail.com>
2025-06-09 16:58:25 +0200

    lcio example added

---
+++ k4ProjectTemplate/options/samples_lcio.py
@@ -0,0 +1,14 @@
+target_lumi = 137000  # in /pb
+
+samples = [
+    {
+        "name": "2f_lep",
+        "id": 2,
+        "xsec": 6077.22,
+        "nevents": 600,
+        "files": [
+            "/afs/cern.ch/user/c/chensel/ILD/lcio_edm4hep/edm4hep/lcio_background_test.root"
+        ]
+    },
+]
+
